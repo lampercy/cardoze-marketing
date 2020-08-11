ab8686311b203ec6d6c7f86aa075504cd8dedd29
Percy Lam <percy.lam@ormuco.com>
2020-08-10 21:51:51 -0400

    add build

---
+++ dist/precache-manifest.887b2bb45eb142974476e22cf00c6cf8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e586924e63eff8ccc9",
+    "url": "/cardoze-marketing/dist/css/app.8a08625d.css"
+  },
+  {
+    "revision": "7f3d0051efe27da08d9e",
+    "url": "/cardoze-marketing/dist/css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "af62a1d91a5b106509fbb5f960a0af04",
+    "url": "/cardoze-marketing/dist/img/BG.af62a1d9.png"
+  },
+  {
+    "revision": "23e450e55dc79d50cc3eea8fc7e7fb60",
+    "url": "/cardoze-marketing/dist/img/F1.23e450e5.png"
+  },
+  {
+    "revision": "6cf13c7fb8ff912c31fee07e09dd55c0",
+    "url": "/cardoze-marketing/dist/img/F2.6cf13c7f.png"
+  },
+  {
+    "revision": "5c51f8f3212b050c5df0123748af120d",
+    "url": "/cardoze-marketing/dist/img/F3.5c51f8f3.png"
+  },
+  {
+    "revision": "0360db88e307e49f5c9a3e403aaa96fb",
+    "url": "/cardoze-marketing/dist/img/F4.0360db88.png"
+  },
+  {
+    "revision": "5614f944a723e789b6af18bacef58c06",
+    "url": "/cardoze-marketing/dist/img/Phone.5614f944.png"
+  },
+  {
+    "revision": "2ce5e443e2a98556e56acbcd53ff9314",
+    "url": "/cardoze-marketing/dist/img/Students.2ce5e443.png"
+  },
+  {
+    "revision": "3563b26194f4b32d2b24d8b48ddf6ca9",
+    "url": "/cardoze-marketing/dist/img/cardoze.3563b261.png"
+  },
+  {
+    "revision": "fdc21d6d6a96ef3cf5db87d6aa3023a5",
+    "url": "/cardoze-marketing/dist/img/learn more.fdc21d6d.png"
+  },
+  {
+    "revision": "8afce45ee890410e5f149dd40a35c668",
+    "url": "/cardoze-marketing/dist/index.html"
+  },
+  {
+    "revision": "b8e586924e63eff8ccc9",
+    "url": "/cardoze-marketing/dist/js/app.670615f4.js"
+  },
+  {
+    "revision": "7f3d0051efe27da08d9e",
+    "url": "/cardoze-marketing/dist/js/chunk-vendors.04f2d793.js"
+  },
+  {
+    "revision": "f019313de9028b59c4544980fcbea96d",
+    "url": "/cardoze-marketing/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cardoze-marketing/dist/robots.txt"
+  }
+]);
