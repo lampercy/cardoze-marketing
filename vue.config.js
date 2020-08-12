3de3f274ad9b4ec461cd96d831114dd89118ef0c
Percy Lam <percy.lam@ormuco.com>
2020-08-12 00:56:04 -0400

    update public path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "http://www.cardozeapp.com/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "https://cardozeapp.com/" : "/"
 };
